06beb6f15783d21fbc071b2712f70303ea1bbfc6
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-07-08 13:48:44 -0500

    added function on script

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,21 @@ function punny() {
    card2.textContent = data.joke
   });
 }
+//SS created a random YoMama joke function
+yomamaJoke ();
+function yomamaJoke () {
+  fetch('https://salty-mountain-68764.herokuapp.com/api.yomomma.info/')
+  .then(function(response){
+    return response.json();
+  }).then(function(data){
+  
+    console.log(data)
+  
+  console.log("joke", data.joke)
+    var card4 = document.getElementById("card-content4")
+    card4.textContent = data.joke;
+  });
+}
     
     
 
