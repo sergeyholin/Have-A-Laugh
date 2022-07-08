a8b975a690620d3269b8a583bc5a71850f071634
Sergey Holin <holin_sergey@yahoo.com>
2022-07-07 21:58:04 -0700

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,23 @@ fetch("https://api.chucknorris.io/jokes/random")
 });
 }
 
+function ranndomJoke () {
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+    .then(function(response){
+      return response.json();
+    }).then(function(data){
+    // Getting data from API
+      console.log(data)
+    //   Picking data that i want
+    //   console.log("joke", data.value)
+    console.log("joke", data.joke)
+
+    // Appending dynamicaly joke to card body element on HTML
+      var card = document.getElementById("card-content1")
+      card.textContent = data.value;
+    });
+}
+    
+    
+
 
