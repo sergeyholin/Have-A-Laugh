8e1b59c9a3b2b78c4e8b30ed9c1342f325198254
Sergey Holin <holin_sergey@yahoo.com>
2022-07-08 11:38:13 -0700

    changed JS source on html to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,38 +15,55 @@ fetch("https://api.chucknorris.io/jokes/random")
 });
 }
 
-function randomJoke () {
-    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
-    .then(function(response){
-      return response.json();
-    }).then(function(data){
-    // Getting data from API
-      console.log(data)
-    //   Picking data that i want
-    //   console.log("joke", data.value)
-    console.log("joke", data.joke)
+// function randomJoke () {
+//     fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
+//     .then(function(response){
+//       return response.json();
+//     }).then(function(data){
+//     // Getting data from API
+//       console.log(data)
+//     //   Picking data that i want
+//     //   console.log("joke", data.value)
+//     console.log("joke", data.joke)
 
-    // Appending dynamicaly joke to card body element on HTML
-      var card = document.getElementById("card-content1")
-      card.textContent = data.joke;
-    });
-}
+//     // Appending dynamicaly joke to card body element on HTML
+//       var card = document.getElementById("card-content1")
+//       card.textContent = data.joke;
+//     });
+// }
 
 
 
-// AH created a random Pun joke function
-function punny() {
-  fetch("https://v2.jokeapi.dev/joke/Pun?type=single")
-  .then(function(response){return response.json();})
+// // AH created a random Pun joke function
+// function punny() {
+//   fetch("https://v2.jokeapi.dev/joke/Pun?type=single")
+//   .then(function(response){return response.json();})
+//   // Getting data from API
+//   .then(function(data){console.log(data)
+//   // Choosing specific data from API 
+//     console.log("punny", data.joke)
+//   // Appending Pun joke to Card2 element on HTML
+//    var card2 = document.getElementById("card-content2")
+//    card2.textContent = data.joke
+//   });
+// }
+
+// function randomJoke () {
+  fetch("https://icanhazdadjoke.com/")
+  .then(function(response){
+    return response.json();
+  }).then(function(data){
   // Getting data from API
-  .then(function(data){console.log(data)
-  // Choosing specific data from API 
-    console.log("punny", data.joke)
-  // Appending Pun joke to Card2 element on HTML
-   var card2 = document.getElementById("card-content2")
-   card2.textContent = data.joke
+    console.log(data)
+  //   Picking data that i want
+  //   console.log("joke", data.value)
+  // console.log("joke", data.joke)
+
+  // // Appending dynamicaly joke to card body element on HTML
+  //   var card = document.getElementById("card-content1")
+  //   card.textContent = data.joke;
   });
-}
+// }
     
     
 
