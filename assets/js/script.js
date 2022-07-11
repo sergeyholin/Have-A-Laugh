e6bb4b41a24ced5733a8ebba73458b0135062291
Sergey Holin <holin_sergey@yahoo.com>
2022-07-11 09:25:30 -0700

    made save the joke to local storage function, made function to populate index with saved joke from local storage array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-
-// SH Made a random chuck joke function
-function chuckJoke () {
-fetch("https://api.chucknorris.io/jokes/random")
-.then(function(response){
-  return response.json();
-}).then(function(data){
-// Getting data from API
-  console.log(data)
-//   Picking data that i want
-  console.log("joke", data.value)
-// Appending dynamicaly joke to card body element on HTML
-  var card = document.getElementById("card-content1")
-  card.textContent = data.value;
-});
-}
-
 // function randomJoke () {
 //     fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=single")
 //     .then(function(response){
@@ -32,38 +15,17 @@ fetch("https://api.chucknorris.io/jokes/random")
 //     });
 // }
 
-
-
-// // AH created a random Pun joke function
-// function punny() {
-//   fetch("https://v2.jokeapi.dev/joke/Pun?type=single")
-//   .then(function(response){return response.json();})
-//   // Getting data from API
-//   .then(function(data){console.log(data)
-//   // Choosing specific data from API 
-//     console.log("punny", data.joke)
-//   // Appending Pun joke to Card2 element on HTML
-//    var card2 = document.getElementById("card-content2")
-//    card2.textContent = data.joke
-//   });
-// }
-
-// function randomJoke () {
-  fetch("https://icanhazdadjoke.com/")
-  .then(function(response){
-    return response.json();
-  }).then(function(data){
-  // Getting data from API
-    console.log(data)
-  //   Picking data that i want
-  //   console.log("joke", data.value)
-  // console.log("joke", data.joke)
-
-  // // Appending dynamicaly joke to card body element on HTML
-  //   var card = document.getElementById("card-content1")
-  //   card.textContent = data.joke;
-  });
-// }
+// SH Added local storage function for appending saved jokes.
+// local storage function========================================================================================
+// Declaring global variable for local storage array
+var display = JSON.parse(localStorage.getItem("joke"))
+console.log(display);
+// Made a loop to append one joke at the time from local storage array
+for (var i = 0; i < display.length; i++) {
+console.log("loop started");
+console.log((display[i]));
+$("#local-storage").append((`<button class="btn btn-secondary btn-block" id="city-button"><strong>${display[i]}</strong></button>`));    
+}
     
     
 
