1ff448f2f5ec81c3be2c7a3cb94fb619ec44f637
aaronhdm <aaronhdm@gmail.com>
2022-07-12 15:01:51 -0500

    modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,11 +97,7 @@ if (display) {
   for (var j = 0; j < display.length; j++) {
     console.log("loop started");
     console.log((display[j]));
-<<<<<<< HEAD
-    $("#local-storage").append((`<button style=";padding: 5px; background-color: white;"><strong>${display[j]}</strong></button>`));    
-=======
-    $("#local-storage").append((`<button style=";padding: 5px; margin-top: 10px; border-radius: 16px; background-color: #FAEA48;"><strong>${display[j]}</strong></button>`));    
->>>>>>> 1eb2e51eff9929fc644d95cae5514817e53bc369
+    $("#local-storage").append((`<button style=";padding: 5px; margin-top: 10px; border-radius: 16px; background-color: white;"><strong>${display[j]}</strong></button>`));    
     }
 }
 };  
